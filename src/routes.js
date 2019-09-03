9218110897475cc2f95ae9642411a1c2b2bea713
leodaiub <leodaiub@gmail.com>
2019-09-02 22:50:42 -0300

    uploads working

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,10 @@ const multerConfig = require("./config/upload");
 
 const routes = new express.Router();
 
-routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
-    const { originalname: name, size, key, location: url = "" } = req.file;
+routes.post("/posts", multer(multerConfig).array('superMarketAdditionalImages', 2), async (req, res) => {
+  
+    
+    const { originalname: name, size, key, location: url = "" } = req.files[1];
   
     const post = await Post.create({
       name,
